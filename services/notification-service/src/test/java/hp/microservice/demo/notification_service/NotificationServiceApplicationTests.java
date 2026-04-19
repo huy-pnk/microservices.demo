929b5a35c023b40f18752e95911f86eec639329b
pnkkhuy <khanhhuy215@gmail.com>
2026-04-19 22:03:09 +0700

    Init all services
    - `services/api-gateway/`
    - `services/user-service/`
    - `services/product-service/`
    - `services/order-service/`
    - `services/inventory-service/`
    - `services/notification-service/`

---
+++ services/notification-service/src/test/java/hp/microservice/demo/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hp.microservice.demo.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
